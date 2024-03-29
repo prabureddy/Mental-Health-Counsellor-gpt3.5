58f7c20964d9b8d1203e4b1bf55da9b460f88928
Bhargava Prabu Reddy <prabu0reddy777@gmail.com>
2024-03-29 14:59:40 -0500

    initial commit

---
+++ pages/api/get-counselling.js
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { OpenAI } from "openai";
+
+const openai = new OpenAI(
+  { organization: 'org-EYj0pavhRG82Sw5VjhfQ4ga0', apiKey: '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' }
+);
+
+function getCounselling(prompt) {
+  return new Promise(async (res) => {
+
+    const response = await openai.chat.completions.create({
+      messages: [{ role: "user", content: `You are mental health counselor and one of your patient is asking you this question: \n\n\n ${prompt} \n\n\n What should you tell them?` }],
+      model: "gpt-3.5-turbo",
+    });
+
+    res(response.choices[0]?.message?.content);
+  })
+}
+
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { body } = req;
+    const { prompt } = body;
+    const result = await getCounselling(prompt);
+    res.status(200).json({ result });
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
