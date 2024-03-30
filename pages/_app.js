7d0debed7d57e069c0be1b2126589a4cdcbc2d13
Bhargava Prabu Reddy <prabu0reddy777@gmail.com>
2024-03-30 11:44:20 -0500

    Updated UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "@/styles/globals.css";
 
-import './index.css'
+import "./index.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <NextUIProvider>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </NextUIProvider>
     </Html>
   );
 }
